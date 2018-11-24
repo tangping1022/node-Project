3671f43d57fec15fd7cafb72c465fd880cdc1dec
tangping1022 <619815085@qq.com>
2018-11-24 09:01:16 +0800

    frist commit

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+router.get('/users.html', function (req, res) {
+  res.render('users');
+})
+router.get('/brand.html', function (req, res) {
+  res.render('brand');
+})
+router.get('/phone.html', function (req, res) {
+  res.render('phone');
+})
+module.exports = router;
